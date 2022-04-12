24630e8746d259304cae0cda6ba0bcd01eb20949
alevaldiviezo <alevadiviezo@gmail.com>
2022-04-12 11:24:16 +0100

    Test authentication

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
 import { Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons} from '@expo/vector-icons';
-
+import { initializeApp } from 'firebase/app';
+import {getAuth, onAuthStateChanged, signInWithEmailAndPassword} from "firebase/auth";
+// import * as firebase from "firebase";
 import {useFonts as useOswald, Oswald_400Regular} from '@expo-google-fonts/oswald';
 
 import {useFonts as useLato, Lato_400Regular} from '@expo-google-fonts/lato';
@@ -17,10 +19,42 @@ import { LocationContextProvider } from './src/services/location/location.contex
 import {FavouritesContextProvider } from './src/services/favourites/favourites.context'
 import { Navigation } from './src/infraStructure/navigation';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCmiXgchu_Wwb9XrHT0jwgzrzTHcF3Woe0",
 
+  authDomain: "mealstogo-d8fb8.firebaseapp.com",
+
+  projectId: "mealstogo-d8fb8",
+
+  storageBucket: "mealstogo-d8fb8.appspot.com",
+
+  messagingSenderId: "901493205885",
+
+  appId: "1:901493205885:web:aa2d0a10782e31c659fa14"
+
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export default function App() {
 
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+  useEffect(() => {
+    setTimeout(() => { 
+    signInWithEmailAndPassword(auth,'test@meal.com', 'test123')
+      
+      .then((user) => {
+        console.log(user);
+        setIsAuthenticated(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+    }, 3000);
+    }, [])
+  
   // const isAndroid = Platform.OS === 'android';
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
